2643ef2ae9517e7ddaf03580f543ac9fe8357bc8
LeandroRoberto <leandroroberto.br@gmail.com>
2014-12-07 13:33:09 -0200

    commit

--- ime/src/br/edu/ifg/ime/ImeWorkspace.java
+++ ime/src/br/edu/ifg/ime/ImeWorkspace.java
@@ -490,12 +490,16 @@ public class ImeWorkspace implements Serializable {
 
 		repositorio.add(ld);
 		return ld;
+
 	}
-	
+
 	public void toogleFlagTreeView(String _identifier) {
+
 		for (LdProject ld : repositorio) {
+
 			if (ld == null)
 				return;
+
 			if (ld.getIdentifier().equals(_identifier))		{	
 				ld.flagTreeView = !ld.flagTreeView;
 
